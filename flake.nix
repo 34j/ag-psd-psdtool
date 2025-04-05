da3f7840d923926e0e7b5ecca3b12f574fc3ec37
34j <34j.github@proton.me>
2025-04-05 14:52:39 +0900

    fix(flake.nix): wip

--- flake.nix
+++ flake.nix
@@ -11,9 +11,10 @@
       let
         pkgs = nixpkgs.legacyPackages.${system};
         lib = pkgs.lib;
-        libPath = lib.makeLibraryPath (
-          with pkgs;
-          [
+        nodeFHS = pkgs.buildFHSEnv {
+          name = "pnpm";
+          runScript = "${pkgs.pnpm}/bin/pnpm";
+          targetPackages = with pkgs; [
             openssl
             systemd
             glib
@@ -37,41 +38,18 @@
             cairo
             nspr
             libgbm
-          ]
-        );
+            libuuid
+            freetype
+            nodejs
+          ];
+        };
         wrapPrefix = if (!pkgs.stdenv.isDarwin) then "LD_LIBRARY_PATH" else "DYLD_LIBRARY_PATH";
-        patchedpnpm = (
-          pkgs.symlinkJoin {
-            name = "pnpm";
-            paths = [ pkgs.pnpm ];
-            buildInputs = [ pkgs.makeWrapper ];
-            postBuild = ''
-              wrapProgram "$out/bin/pnpm" --prefix ${wrapPrefix} : "${libPath}"
-            '';
-          }
-        );
-        patchednode = (
-          pkgs.symlinkJoin {
-            name = "node";
-            paths = [ pkgs.nodejs ];
-            buildInputs = [ pkgs.makeWrapper ];
-            postBuild = ''
-              wrapProgram "$out/bin/node" --prefix ${wrapPrefix} : "${libPath}"
-            '';
-          }
-        );
       in
       {
         devShells.default = pkgs.mkShell {
-          packages =
-            (with pkgs; [
-              # project dependencies
-              pre-commit
-            ])
-            ++ [
-              patchednode
-              patchedpnpm
-            ];
+          packages = [
+            nodeFHS
+          ];
         };
       }
     );
