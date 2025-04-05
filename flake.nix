86c546ed90d121fcfc015e64ce977b50f97e0391
34j <34j.github@proton.me>
2025-04-05 14:53:15 +0900

    revert(flake.nix): revert flake.nix changes

--- flake.nix
+++ flake.nix
@@ -10,47 +10,53 @@
       system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
-        lib = pkgs.lib;
-        nodeFHS = pkgs.buildFHSEnv {
-          name = "pnpm";
-          runScript = "${pkgs.pnpm}/bin/pnpm";
-          targetPackages = with pkgs; [
-            openssl
-            systemd
-            glib
-            cups
-            nss
-            alsa-lib
-            dbus
-            at-spi2-core
-            libdrm
-            expat
-            xorg.libX11
-            xorg.libXcomposite
-            xorg.libXdamage
-            xorg.libXext
-            xorg.libXfixes
-            xorg.libXrandr
-            xorg.libxcb
-            mesa
-            libxkbcommon
-            pango
-            cairo
-            nspr
-            libgbm
-            libuuid
-            freetype
-            nodejs
-          ];
-        };
-        wrapPrefix = if (!pkgs.stdenv.isDarwin) then "LD_LIBRARY_PATH" else "DYLD_LIBRARY_PATH";
       in
       {
-        devShells.default = pkgs.mkShell {
-          packages = [
-            nodeFHS
-          ];
-        };
+        devShells.default =
+          (pkgs.buildFHSEnv {
+            name = "playwright";
+            targetPkgs =
+              pkgs:
+              [
+                # playwright (firefox) dependencies
+                (pkgs.runCommand "steamrun-lib" { }
+                  "mkdir $out; ln -s ${pkgs.steam-run-free.fhsenv}/usr/lib64 $out/lib"
+                )
+              ]
+              ++ (with pkgs; [
+                # playwright (chromium) dependencies
+                # https://zenn.dev/link/comments/d456595abd7aea
+                openssl
+                systemd
+                glib
+                cups
+                nss
+                alsa-lib
+                dbus
+                at-spi2-core
+                libdrm
+                expat
+                xorg.libX11
+                xorg.libXcomposite
+                xorg.libXdamage
+                xorg.libXext
+                xorg.libXfixes
+                xorg.libXrandr
+                xorg.libxcb
+                mesa
+                libxkbcommon
+                pango
+                cairo
+                nspr
+                libgbm
+              ])
+              ++ (with pkgs; [
+                # project dependencies
+                pnpm
+                nodejs
+                pre-commit
+              ]);
+          }).env;
       }
     );
 }
