ed377cb8e809944cb393cf248274f44a731ae978
34j <34j.github@proton.me>
2025-04-05 16:39:12 +0900

    test: add more tests

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -3,17 +3,23 @@ import { isNode } from 'browser-or-node'
 import { describe, it } from 'vitest'
 import { renderPsd } from '../src/index'
 
-describe('index', () => {
-  describe('psdToTypes', () => {
+describe('index', async () => {
+  const request = await fetch('http://127.0.0.1:8080/ccchu.psd')
+  const buffer = await request.arrayBuffer()
+  if (isNode) {
+    // eslint-disable-next-line ts/no-require-imports
+    require('ag-psd/initialize-canvas')
+  }
+  const psd = readPsd(buffer)
+
+  describe.each([
+    { flipx: false, flipy: false },
+    { flipx: true, flipy: false },
+    { flipx: false, flipy: true },
+    { flipx: true, flipy: true },
+  ])('renderPsd', (options) => {
     it('should able to write files', async () => {
-      const request = await fetch('http://127.0.0.1:8080/ccchu.psd')
-      const buffer = await request.arrayBuffer()
-      if (isNode) {
-        // eslint-disable-next-line ts/no-require-imports
-        require('ag-psd/initialize-canvas')
-      }
-      const psd = readPsd(buffer)
-      const canvas = renderPsd(psd, {})
+      const canvas = renderPsd(psd, {}, options)
       if (isNode) {
         const { writeFile } = (await import('node:fs')).promises
         const Buffer = (await import('node:buffer')).Buffer
